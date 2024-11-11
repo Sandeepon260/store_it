dfb8b84cdcc4fd35f4024aae1609b7f45d77dda7
Sandeepon260 <sandeepon.saha@gmail.com>
2024-11-11 11:33:13 -0500

    app-write sign up

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,6 +17,7 @@ import {Input} from "@/components/ui/input";
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { createAccount } from "@/lib/actions/user.actions";
 
 
 
@@ -31,6 +32,7 @@ const authFormSchema = (formType: FormType) => {
 const AuthForm = ({type}:{type: FormType}) => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [accountId, setAccountId] = useState(null);
   const formSchema = authFormSchema(type);
     const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -39,7 +41,20 @@ const AuthForm = ({type}:{type: FormType}) => {
     },
   })
   const onSubmit = async(values: z.infer<typeof formSchema>) =>{
-    console.log(values);
+    setIsLoading(true);
+    setErrorMessage("");
+    try {
+      const user = await createAccount({
+        fullName: values.fullName || "",
+        email: values.email
+      })
+      setAccountId(user.accountId);
+    } catch (error) {
+      setErrorMessage("Failed to create account. Please try again.")
+    } finally {
+      setIsLoading(false)
+    }
+    
   };
     return (
         <>
