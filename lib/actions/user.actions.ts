bdedaf765d85fada6eb7bf61f2a2b1f4a8ab30c3
Sandeepon260 <sandeepon.saha@gmail.com>
2024-11-12 12:00:27 -0500

    mobileNav - Sidebar - header

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 "use server"
 
 import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "../appwrite"
+import { createAdminClient, createSessionClient } from "../appwrite"
 import { appwriteConfig } from "../appwrite/cofig";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
 
 
 const getUserByEmail = async(email: string) => {
@@ -44,7 +45,7 @@ export const createAccount = async({fullName, email} : {fullName: string, email:
             {
                 fullName,
                 email,
-                avatar:"/assets/avatar.png",
+                avatar:avatarPlaceholderUrl,
                 accountId,
 
             }
@@ -68,4 +69,19 @@ export const verifySecret = async({accountId, password}: {accountId:string, pass
         handleError(error, "Failed to verify OTP");
     }
     
+};
+
+export const getCurrentUser = async() => {
+    const { databases, account} = await createSessionClient();
+    const result = await account.get();
+    const user = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.usersCollectionId,
+        [Query.equal("accountId", [result.$id])],
+    );
+
+    if(user.total <= 0) return null;
+
+    return parseStringify(user.documents[0]);
+
 }
