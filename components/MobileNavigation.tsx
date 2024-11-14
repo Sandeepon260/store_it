77ea72dbe75737ad09f0906789669de081cafa31
Sandeepon260 <sandeepon.saha@gmail.com>
2024-11-13 22:08:00 -0500

    file-uploader

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -93,6 +93,7 @@ const MobileNavigation = ({ ownerId, accountId, fullName, avatar, email}: Props)
         <Separator className="my-5 bg-light-200/20"/>
         <div className="flex flex-col justify-between gap-5 pb-5">
           {/* s */}
+          <FileUploader ownerId={ownerId} accountId={accountId} />
           <Button type="submit" className='mobile-sign-out-button' onClick={async() => await signOutUser()}>
                     <Image 
                         src="/assets/icons/logout.svg"
